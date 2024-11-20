e5bcbe5607813d62049788af8fbb9746cdf48970
aditya <adityamishra1711@gmailcom>
2024-11-20 23:26:02 +0530

    initial

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1> here we are </h1>
+      <Button>
+        click here
+      </Button>
     </div>
   );
 }
